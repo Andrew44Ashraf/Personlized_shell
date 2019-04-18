a35d0b9f5d66e46aa11ab66e46e5f69cec8fdd19
Andrew44Ashraf <andrew.achraf44@gmail.com>
2019-04-18 16:29:36 +0200

    Add files via upload
    parsing class

---
+++ tokens.py
@@ -0,0 +1,66 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+
+class Parsing(object):
+
+    specialCharacters = ['>', '<','|','*']
+
+    def __init__(self,text):
+        self.line = text
+        self.commandsArgument = []
+        self.specialChar = 0
+        self.commands = ''
+        self.arg = []
+
+    def tokens(self):
+        self.commandsArgument = word_tokenize(self.line)
+        self._seperateCommandsArgs()
+        self.getCommandsArgsExc()
+  
+   
+
+    def _seperateCommandsArgs(self):
+        self.commands= self.commandsArgument[0]
+        self.arg =self.commandsArgument[1:]
+
+    def excutable(self):
+        if self.commands[0]== '.' and self.commands[1] == '/':
+            return False 
+        else : 
+            return True 
+
+    def specialChars (self):
+        for word in self.commandsArgument:
+            if word in self.specialCharacters:
+                return True , word  
+        else: 
+            return False 
+
+    def getCommandsArgsExc(self):
+        return  self.excutable(), self.commands , self.arg
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__" :
+
+    text = './sss |'
+
+    par = Parsing(text)
+    par.tokens()
+    
+    x,c,r = par.getCommandsArgsExc()
+    print(str(x)+' '+str(c)+' '+str(r))
+    ss = par.specialChars()
+    print(str(ss))
+    
+    
